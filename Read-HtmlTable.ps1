9141088e2bc997a4ee102cf8a15ff751ee9dc9bc
iRon7 <boderonald@gmail.com>
2021-04-20 11:54:06 +0200

    Update Read-HtmlTable.ps1

--- Read-HtmlTable.ps1
+++ Read-HtmlTable.ps1
@@ -1,6 +1,7 @@
 Function Read-HtmlTable {
     [CmdletBinding()][OutputType([Object[]])] param(
-        [Parameter(ValueFromPipeLine = $True, Mandatory = $True)][String]$Html
+        [Parameter(ValueFromPipeLine = $True, Mandatory = $True)][String]$Html,
+        [Int[]]$TableIndex
     )
     Begin {
         Function Get-Elements { # Get closed descendants by tag name
@@ -16,16 +17,20 @@ Function Read-HtmlTable {
         $Unicode = [System.Text.Encoding]::Unicode.GetBytes($Html)
         $Document = New-Object -Com 'HTMLFile'
         if ($Document.IHTMLDocument2_Write) { $Document.IHTMLDocument2_Write($Unicode) } else { $Document.write($Unicode) }
+        $Index = 0 
         foreach($table in ($Document.Body | Get-Elements 'table')) {
-            $Names = $Null
-            foreach ($tr in ($table | Get-Elements 'tr')) {
-                if (!$Names) { $Names = ($tr | Get-Elements 'th').innerText }
-                if (!$Names) { $Names = ($tr | Get-Elements 'td').innerText }
-                else {
-                    $Values = ($tr | Get-Elements 'td').innerText
-                    $Properties = @{}; $i = 0
-                    Foreach ($Value in $Values) { $Properties[$Names[$i++]] = $Value }
-                    [pscustomobject]$Properties
+            if (!$PSBoundParameters.ContainsKey('TableIndex') -or $Index++ -In $TableIndex) {
+                $Names = $Null
+                foreach ($tr in ($table | Get-Elements 'tr')) {
+                    if (!$Names) { $Names = ($tr | Get-Elements 'th').innerText }
+                    if (!$Names) { $Names = ($tr | Get-Elements 'td').innerText }
+                    else {
+                        $Values = ($tr | Get-Elements 'td').innerText
+                        $Properties = [Ordered]@{}
+                        $Count = [Math]::Min($Names.Count, $Values.Count)
+                        for ($i = 0; $i -lt $Count; $i++) { $Properties[$Names[$i]] = $Values[$i] }
+                        if ($Properties.Count -gt 0) { [pscustomobject]$Properties }
+                    }
                 }
             }
         }
