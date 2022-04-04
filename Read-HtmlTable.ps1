640b075ddd5669a6ec1041b516cca64f8133c808
iRon7 <boderonald@gmail.com>
2022-04-04 19:13:46 +0200

    Several fixes and enhancements

--- Read-HtmlTable.ps1
+++ Read-HtmlTable.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 1.0.6
+.VERSION 2.0.0
 .GUID 6ddb4b24-29bc-4268-a62f-402b3ee28e3d
 .AUTHOR iRon
 .COMPANYNAME
@@ -48,23 +48,27 @@ PSCustomObject[]
     If you enter more column headers than there are data columns, the additional column headers are created
     with empty data columns.
 
+    A $Null instead of a column name, will span the respective column with previous column.
+
+    Note: To select specific columns or skip any data (or header) rows, use Select-Object cmdlet
+
 .PARAMETER TableIndex
     Specifies which tables should be selected from the html content (where 0 refers to the first table).
     By default, all tables are extracted from the content.
 
-    Note: the tables headers should be unified to properly output or diplay the tables.
+    Note: in case of multiple tables, the headers should be unified to properly output or display of each table.
     (see: https://github.com/PowerShell/PowerShell/issues/13906)
 
-.PARAMETER HeaderIndex
-    Specifies which header row should be used for the property name. In case the header resides in the same
-    table block container as the data, the data is presumed to start right after the last header index.
-    Default: 0, meaning the first row in the table is presumed the header row
+.PARAMETER Separator
+    Specifies the characters used to join a header with is spanned over multiple columns
+    (default: space character)
 
-    Note: To skip any data rows, use `|Select-Object -Skip ...`.
+.PARAMETER Delimiter
+    Specifies the characters used to join a header with is spanned over multiple rows
+    (default: the newline characters used by the operating system)
 
-.PARAMETER DefaultNameFormat
-    The format used for in case there is no header defined in the HTML table. Where "{0}" refers to the column index.
-    Default: 'Column {0}'
+.PARAMETER NoTrim
+    By default, all header - and data text is trimmed, to disable automatic trimming, use the -NoTrim parameter.
 
 .EXAMPLE
 
@@ -86,85 +90,133 @@ PSCustomObject[]
 [CmdletBinding(DefaultParameterSetName='Html')][OutputType([Object[]])] param(
     [Parameter(ParameterSetName='Html', ValueFromPipeLine = $True, Mandatory = $True, Position = 0)][String]$InputObject,
     [Parameter(ParameterSetName='Uri', ValueFromPipeLine = $True, Mandatory = $True)][Uri]$Uri,
-    [String[]]$Header,
+    [Object[]]$Header,
     [Int[]]$TableIndex,
-    [Int[]]$HeaderIndex = 0,
-    [String]$DefaultNameFormat = 'Column {0}'
+    [String]$Separator = ' ',
+    [String]$Delimiter = [System.Environment]::NewLine,
+    [Switch]$NoTrim
 )
 Begin {
-    function Get-TopElements {
-        [CmdletBinding()][OutputType([__ComObject[]])] param(
-            [Parameter(Mandatory = $True)][String[]]$TagName,
-            [Parameter(Mandatory = $True, ValueFromPipeLine = $True)]$Element
-        )
-        Process {
-            if ($TagName -Contains $Element.tagName) { $Element }
-            else { $Element.Children |Where-Object { $_ } |Foreach-Object { $_ |Get-TopElements $TagName } }
+    function ParseHtml($String) {
+        $Unicode = [System.Text.Encoding]::Unicode.GetBytes($String)
+        $Html = New-Object -Com 'HTMLFile'
+        if ($Html.PSObject.Methods.Name -Contains 'IHTMLDocument2_Write') { $Html.IHTMLDocument2_Write($Unicode) } else { $Html.write($Unicode) }
+        $Html.Close()
+        $Html
+    }
+    filter GetTopElement([String[]]$TagName) {
+        if ($TagName -Contains $_.tagName) { $_}
+        else { @($_.Children).Where{ $_ } | GetTopElement -TagName $TagName }
+    }
+    function GetUnit($Data, [int]$x, [int]$y) {
+        if ($x -lt $Data.Count -and $y -lt $Data[$x].Count) { $Data[$x][$y] }
+    }
+    function SetUnit($Data, [int]$x, [int]$y, [HashTable]$Unit) {
+        while ($x -ge $Data.Count) { $Data.Add([System.Collections.Generic.List[HashTable]]::new()) }
+        while ($y -ge $Data[$x].Count) { $Data[$x].Add($Null) }
+        $Data[$x][$y] = $Unit
+    }
+    function GetData([__ComObject[]]$TRs) {
+        $Data = [System.Collections.Generic.List[System.Collections.Generic.List[HashTable]]]::new()
+        $y = 0
+        foreach($TR in $TRs) {
+            $x = 0
+            foreach($TD in ($TR |GetTopElement 'th', 'td')) {
+                while ($True) { # Skip any row spans
+                    $Unit = GetUnit -Data $Data -x $x -y $y
+                    if (!$Unit) { break }
+                    $x++
+                }
+                $Text = if ($Null -ne $TD.innerText) { if ($NoTrim) { $TD.innerText } else { $TD.innerText.Trim() } }
+                for ($r = 0; $r -lt $TD.rowspan; $r++) {
+                    $y1 = $y + $r
+                    for ($c = 0; $c -lt $TD.colspan; $c++) {
+                        $x1 = $x + $c
+                        $Unit = GetUnit -Data $Data -x $x1 -y $y1
+                        if ($Unit) { SetUnit -Data $Data -x $x1 -y $y1 -Unit @{ ColSpan = $True; Text = $Unit.Text, $Text } } # RowSpan/ColSpan overlap
+                        else { SetUnit -Data $Data -x $x1 -y $y1 -Unit @{ ColSpan = $c -gt 0; RowSpan = $r -gt 0; Text = $Text } }
+                    }
+                }
+                $x++
+            }
+            $y++
         }
+        ,$Data
     }
 }
 Process {
     if (!$Uri -and $InputObject.Length -le 2048 -and ([Uri]$InputObject).AbsoluteUri) { $Uri = [Uri]$InputObject }
-    if ($Uri.AbsoluteUri) { $InputObject = [System.Net.Webclient]::New().DownloadString($Uri) }
-    $Unicode = [System.Text.Encoding]::Unicode.GetBytes($InputObject)
-    $Document = New-Object -Com 'HTMLFile'
-    if ($Document.IHTMLDocument2_Write) { $Document.IHTMLDocument2_Write($Unicode) } else { $Document.write($Unicode) }
-    $Document.Close()
-    $Index = 0
-    foreach($Table in ($Document.Body |Get-TopElements 'table')) {
-        if (!$PSBoundParameters.ContainsKey('TableIndex') -or $Index++ -In $TableIndex) {
-            $THead = $Table |Get-TopElements 'thead'
-            $TableHead = if ($THead) { $THead } else { $Table }
-            $HeadRows = @($TableHead |Get-TopElements 'tr')
-            $TBody = $Table |Get-TopElements 'tbody'
-            $TableBody = if ($TBody) { $TBody } else { $Table }
-            $BodyRows = @($TableBody |Get-TopElements 'tr')
-            $Names = [System.Collections.Generic.List[string]]::new()
-            if ($Header) { $DataIndex = 0 }
-            else {
-                if ($THead -and !$PSBoundParameters.ContainsKey('HeaderIndex')) { $HeaderIndex = 0..($HeadRows.Count - 1) }
-                foreach ($TR in $HeadRows[$HeaderIndex]) {
-                    $i = 0
-                    foreach ($TH in ($TR |Get-TopElements 'th', 'td')) {
-                        for ($ColSpan = 0; $ColSpan -lt $TH.colspan; $ColSpan++) {
-                            if ($i -ge $Names.Count) { $Names.Add($TH.innerText) }
-                            else { $Names[$i] += [Environment]::NewLine + $TH.innerText }
-                            $i++
-                        }
-                    }
-                }
-                for ($i = 0; $i -lt $Names.Count; $i++) { if (!$Names[$i]) { $Names[$i] = $DefaultNameFormat -f ($i + 1) } }
-                $DataIndex = if (!$THead -and $HeadRows.Count -eq $BodyRows.Count) { $HeadRows[$HeaderIndex[-1]].rowIndex + 1 }
+    $Response = if ($Uri -is [Uri] -and $Uri.AbsoluteUri) { Try { Invoke-WebRequest $Uri } Catch { Throw $_ } }
+    $Html = if ($Response) {
+        if ($Response.PSObject.Properties.Name -Contains 'ParsedHtml') { $Response.ParsedHtml }
+        else { ParseHtml $Response.RawContent }
+    } else { ParseHtml $InputObject }
+    $i = 0
+    foreach($Table in ($Html.Body |GetTopElement 'table')) {
+        if (!$PSBoundParameters.ContainsKey('TableIndex') -or $i++ -In $TableIndex) {
+            $Rows = $Table |GetTopElement 'tr'
+            if (!$Rows) { return }
+            if ($PSBoundParameters.ContainsKey('Header')) {
+                $HeadRows = @()
+                $Data = GetData $Rows
             }
-            If ($Names) {
-                $RowSpan = @(1) * ($Names.Count + 1)
-                foreach ($TableRow in $BodyRows) {
-                    if ($TableRow.rowIndex -ge $DataIndex) {
-                        $ColSpan = 1
-                        $Data = $TableRow |Get-TopElements 'th', 'td'
-                        $Data = for ($i = 0; $i -lt $Data.Count) {
-                            $DT = if ($ColSpan -gt 1) { $Data[$i - 1] } elseif ($RowSpan[$i] -gt 1) { $Data1[$i] }
-                            if (--$ColSpan -lt 1) { $ColSpan = $Data[$i].colspan }
-                            if (--$RowSpan[$i] -lt 1) { $RowSpan[$i] = $Data[$i].rowspan }
-                            if ($DT) { $DT } else { $Data[$i++] }
+            else {
+                for ($i = 0; $i -lt $Rows.Count; $i++) { $Rows[$i].id = "id_$i" }
+                $THead = $Table |GetTopElement 'thead'
+                $HeadRows = @(
+                    if ($THead) { $THead |GetTopElement 'tr' }
+                    else { $Rows.Where({ !($_ |GetTopElement 'th') }, 'Until' ) }
+                )
+                if (!$HeadRows -or $HeadRows.Count -eq $Rows.Count) { $HeadRows = $Rows[0] }
+                $Head = GetData $HeadRows
+                $Data = GetData ($Rows.Where{ $_.id -notin $HeadRows.id })
+                $Header = @(
+                    for ($x = 0; $x -lt $Head.Count; $x++) {
+                        if ($Head[$x].Where({ !$_.ColSpan }, 'First') ) {
+                            ,@($Head[$x].Where{ !$_.RowSpan }.ForEach{ $_.Text })
                         }
-                        $Data1 = $Data
-                        $Properties = [Ordered]@{}
-                        for ($i = 0; $i -lt $Names.Count; $i++) {
-                            $Name = $Names[$i]
-                            $Value = if ($i -lt $Data.Count) { $Data[$i].innerText }
-                            if (!$Properties.Contains($Name)) { $Properties[$Name] = $Value }
-                            elseif ($Null -ne $Value) {
-                                if ($Null -eq $Properties[$Name]) { $Properties[$Name] = $Value }
-                                else { $Properties[$Name] = @($Properties[$Name]) + $Value }
-                            }
-                        }
-                        if ($Properties.Count -gt 0) { [pscustomobject]$Properties }
+                        else { $Null } # aka spanned header column
+                    }
+                    for ($x = $Head.Count; $x -lt $Data.Count; $x++) {
+                        if ($Null -ne $Data[$x].Where({ $_ -and !$_.ColSpan }, 'First') ) { '' }
+                    }
+                )
+            }
+            $Header = $Header.ForEach{
+                if ($Null -eq $_) { $Null }
+                else {
+                    $Name = [String[]]$_
+                    $Name = if ($NoTrim) { $Name -Join $Delimiter }
+                            else { (($Name.ForEach{ $_.Trim() }) -Join $Delimiter).Trim() }
+                    if ($Name) { $Name } else { '1' }
+                }
+            }
+            $Unique = [System.Collections.Generic.HashSet[String]]::new([StringComparer]::InvariantCultureIgnoreCase)
+            $Duplicates = @( for ($i = 0; $i -lt $Header.Count; $i++) { if ($Null -ne $Header[$i] -and !$Unique.Add($Header[$i])) { $i } } )
+            $Duplicates.ForEach{
+                do {
+                    $Name, $Number = ([Regex]::Match($Header[$_], '^([\s\S]*?)(\d*)$$')).Groups.Value[1, 2]
+                    $Digits = '0' * $Number.Length
+                    $Header[$_] = "$Name{0:$Digits}" -f (1 + $Number)
+                } while (!$Unique.Add($Header[$_]))
+            }
+            for ($y = 0; $y -lt ($Data |ForEach-Object Count |Measure-Object -Maximum).Maximum; $y++) {
+                $Name = $Null # (custom) -Header parameter started with a spanned ($Null) column
+                $Properties = [ordered]@{}
+                for ($x = 0; $x -lt $Header.Count; $x++) {
+                    $Unit = GetUnit -Data $Data -x $x -y $y -Unit
+                    if ($Null -ne $Header[$x]) {
+                        $Name = $Header[$x]
+                        $Properties[$Name] = if ($Unit) { $Unit.Text } # else $Null (align column overflow)
+                    }
+                    elseif ($Name -and !$Unit.ColSpan) {
+                        $Properties[$Name] = $Properties[$Name], $Unit.Text
                     }
                 }
+                [pscustomobject]$Properties
             }
         }
     }
-    $Null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Document)
+    $Null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Html)
 }
 
